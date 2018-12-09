e1a1377d20d50293557fe1ebc81eb90a405abe80
Brant <boshartb@gmail.com>
2018-12-09 12:20:26 -0500

    SECOND commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import FilmListing from './FilmListing'
+import FilmDetails from './FilmDetails'
+
+import TMDB from './TMDB'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="film-library">
+        <FilmListing films={ TMDB.films } />
+        <FilmDetails films={ TMDB.films } />
+      </div>
+    );
+  }
+}
+
+export default App;
