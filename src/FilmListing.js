dd8567d5d76040723a577d5c208de5f4c8012e23
Brant <boshartb@gmail.com>
2018-12-09 14:30:17 -0500

    part 2 complete

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,16 +3,32 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow'
 
 class FilmListing extends Component {
+
+  handleFilterClick() {
+    console.log("Setting filter to and the filter argument")
+  }
+
   render() {
 
-    let allFilms = this.props.films.map( (film) =>
-      <FilmRow key={ film.id } film={ film } />
+    let allFilms = this.props.films.map((film) =>
+      <FilmRow key={film.id} film={film} />
     )
 
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
-        { allFilms }
+        <div className="film-list-filters">
+          <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+            ALL
+            <span className="section-count">{this.props.films.length}</span>
+          </div>
+          <div className="film-list-filter" onClick={() => this.handleFilterClick('faves')}>
+            FAVES
+            <span className="section-count">0</span>
+          </div>
+        </div>
+
+        {allFilms}
       </div>
     );
   }
