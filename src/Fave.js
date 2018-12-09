3237d40d4ae640e91b30affdafacaa831278dd4f
Brant <boshartb@gmail.com>
2018-12-09 18:37:00 -0500

    Section 4.4 completed event handler state set

--- src/Fave.js
+++ src/Fave.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react';
 //const ListItem = state =>
 class Fave extends Component {
 
+    // 4.1 state with no data
+    state = {
+        // 4.3 Set intital state
+        isFave: false
+    }
+
+    //4.2  Fat arrow to bind context of this
     handleClick = (e) => {
         console.log("handling Fave click!")
         e.stopPropagation()
+
+        //4.4 set state in event handler
+        this.setState((state, props) => {
+            return { isFave: !state.isFave }
+        })
     }
 
     render() {
